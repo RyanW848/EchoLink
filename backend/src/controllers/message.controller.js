ce3d489fa0712b8e952565ea60fc69c4b1849825
Ryan Wong <ryanwong6265@gmail.com>
2025-01-18 20:40:25 -0500

    Added back and forth chatting

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,13 +2,16 @@ import User from "../models/user.model.js"
 import Message from "../models/message.model.js"
 import cloudinary from "../lib/cloudinary.js";
 
-export const getUsers = async (req, res) => {
+export const getUsersForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
-        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");
+        const filteredUsers = await User.find({
+            _id: { $ne: loggedInUserId },
+        }).select("-password");
+
         res.status(200).json(filteredUsers);
     } catch (error) {
-        console.log("Error in getUsers", error.message);
+        console.log("Error in getUsersForSidebar: ", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 };
@@ -16,13 +19,13 @@ export const getUsers = async (req, res) => {
 // Finds all messages between sender and receiver
 export const getMessages = async (req, res) => {
     try {
-        const { id:userToChatId } = req.params;
-        const senderId = req.user_id;
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
         
         const messages = await Message.find({
-            $or:[
-                {senderId: senderId, receiverId: userToChatId},
-                {senderId: userToChatId, receiverId: senderId}
+            $or: [
+                {senderId: myId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: myId}
             ]
         })
 
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.protectroute.js";
-import { getUsers, getMessages, sendMessage } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/users", protectRoute, getUsers);
+router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
