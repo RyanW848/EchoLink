c116213e97d84dbe8939cd0038fee03640d95e5a
Ryan Wong <ryanwong6265@gmail.com>
2025-01-18 19:25:58 -0500

    Added Sidebar

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,24 +2,24 @@ import User from "../models/user.model.js"
 import Message from "../models/message.model.js"
 import cloudinary from "../lib/cloudinary.js";
 
-export const getUsers = async (res, req) => {
+export const getUsers = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
         const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");
         res.status(200).json(filteredUsers);
     } catch (error) {
         console.log("Error in getUsers", error.message);
-        res.staus(500).json({ error: "Internal server error" });
+        res.status(500).json({ error: "Internal server error" });
     }
 };
 
 // Finds all messages between sender and receiver
-export const getMessages = async (res, req) => {
+export const getMessages = async (req, res) => {
     try {
         const { id:userToChatId } = req.params;
         const senderId = req.user_id;
         
-        const meessages = await Message.find({
+        const messages = await Message.find({
             $or:[
                 {senderId: senderId, receiverId: userToChatId},
                 {senderId: userToChatId, receiverId: senderId}
@@ -29,7 +29,7 @@ export const getMessages = async (res, req) => {
         res.status(200).json(messages);
     } catch (error) {
         console.log("Error in getMessages", error.message);
-        res.staus(500).json({ error: "Internal server error" });
+        res.status(500).json({ error: "Internal server error" });
     }
 };
 
@@ -57,6 +57,6 @@ export const sendMessage = async (req, res) => {
         res.status(201).json(newMessage);
     } catch (error) {
         console.log("Error in sendMessage", error.message);
-        res.staus(500).json({ error: "Internal server error" });
+        res.status(500).json({ error: "Internal server error" });
     }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,29 @@
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import { connectDB } from "./lib/db.js"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}));
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
-    console.log("server running on port: " + PORT)
+    console.log("server running on port: " + PORT);
     connectDB();
-})
+});
